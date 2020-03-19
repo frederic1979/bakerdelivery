97851907978079fb4774a1d727b322d8106d70d7
frederic1979 <frederic.buono.devweb@gmail.com>
2020-03-19 17:44:26 +0100

    Last

--- src/main/java/co/simplon/bakerdelivery/controller/RestaurantController.java
+++ src/main/java/co/simplon/bakerdelivery/controller/RestaurantController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -23,31 +24,40 @@ public class RestaurantController {
     @Autowired
     RestaurantService restaurantService;
 
-    @Autowired //Spring créé instance tout seul
+    @Autowired //Spring créé instance tout seul, pas la peine dans RestoMapper de faire :RestaurantMapper INSTANCE = Mappers.getMapper( RestaurantMapper.class );
     RestaurantMapper restaurantMapper;
 
-    /*public RestaurantController(RestaurantService restaurantService) {
-
+    /*Autowired permet :
+    public RestaurantController(RestaurantService restaurantService) {
         this.restaurantService = restaurantService;
     }*/
 
+    //ici j'ai déja implementé, grace à Spring, la methode maps pour transf list de restau en liste de restDto
     @GetMapping
     public List<RestaurantDto> getRestaurants() {
+        return restaurantMapper.maps(restaurantService.getRestaurants());
+    }
+
+
+   /*
+   Ou on traite ici le cas de la liste
+   @GetMapping
+   public List<RestaurantDto> getRestaurants() {
         List<Restaurant> restaurants = restaurantService.getRestaurants();
-        List<RestaurantDto> restaurantDtos = null;
+        List<RestaurantDto> restaurantDtos = new ArrayList<RestaurantDto>( restaurants.size());
         for (Restaurant restaurant : restaurants) {
             RestaurantDto restaurantDto = restaurantMapper.map(restaurant);
             restaurantDtos.add(restaurantDto);
         }
-
+        System.out.println("liste des resto : "+restaurantDtos);
         return restaurantDtos;
-    }
+    }*/
 
 
     @GetMapping("/{restaurantId}")
     public ResponseEntity<RestaurantDto> getRestaurantById(@PathVariable Long restaurantId) {
         Optional<Restaurant> restaurant = restaurantService.getRestaurantById(restaurantId);
-        System.out.println("le nom du resto est" + restaurantMapper.map(restaurant.get()).getName()); //ici on doit préciser qu'on affiche le nom
+        //System.out.println("le nom du resto est" + restaurantMapper.map(restaurant.get()).getName()); //ici on doit préciser qu'on affiche le nom
         if (restaurant.isPresent()) {
 
             return ResponseEntity.ok(restaurantMapper.map(restaurant.get())); //coté front pas besoin, avec {{}} on recoit un objet restau et on decide d'afficher le nom
@@ -56,9 +66,9 @@ public class RestaurantController {
 
 
     @PostMapping
-    public ResponseEntity<Restaurant> createRestaurant(@RequestBody Restaurant restaurant) {
+    public ResponseEntity<RestaurantDto> createRestaurant(@RequestBody Restaurant restaurant) {
         try {
-            return ResponseEntity.ok(restaurantService.createRestaurant(restaurant));
+            return ResponseEntity.ok(restaurantMapper.map(restaurantService.createRestaurant(restaurant)));
 
         } catch (Exception e) {
             return ResponseEntity.badRequest().build();
@@ -67,17 +77,17 @@ public class RestaurantController {
 
 
     @PutMapping("/{restaurantId}")
-    public ResponseEntity<Restaurant> updateRestaurant(@RequestBody Restaurant restaurant, @PathVariable Long restaurantId) {
+    public ResponseEntity<RestaurantDto> updateRestaurant(@RequestBody Restaurant restaurant, @PathVariable Long restaurantId) {
         try {
 
-            return ResponseEntity.ok(restaurantService.updateRestaurant(restaurant, restaurantId));
+            return ResponseEntity.ok(restaurantMapper.map(restaurantService.updateRestaurant(restaurant, restaurantId)));
         } catch (RestaurantNotFoundException e) {
             return ResponseEntity.notFound().build();
         }
     }
 
     @DeleteMapping("/{restaurantId}")
-    public ResponseEntity<Restaurant> deleteRestaurant(@PathVariable Long restaurantId) {
+    public ResponseEntity<RestaurantDto> deleteRestaurant(@PathVariable Long restaurantId) {
 
         if (restaurantService.deleteRestaurant(restaurantId)) {
             return ResponseEntity.noContent().build();
--- src/main/java/co/simplon/bakerdelivery/mappers/RestaurantMapper.java
+++ src/main/java/co/simplon/bakerdelivery/mappers/RestaurantMapper.java
@@ -4,15 +4,19 @@ import co.simplon.bakerdelivery.dto.RestaurantDto;
 import co.simplon.bakerdelivery.model.Restaurant;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 
 @Mapper(componentModel = "spring")
 public interface RestaurantMapper {
 
 
-
-
     @Mapping(source = "id", target = "id")
     RestaurantDto map(Restaurant restaurant);
+
+    List<RestaurantDto> maps(List<Restaurant> restaurants);
+
+    // transf DTO ---> EntityManager
+    Restaurant unmap(RestaurantDto restaurantDto);
 }
